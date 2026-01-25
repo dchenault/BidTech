d26d4951ea1746c2c9dbbfb0e4dd5a0af295d601
Dave Chenault <webschemes.net@gmail.com>
2026-01-25 18:29:32 +0000

    Still no change. Here is the console error when it tries to upload the i

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9555481115-a8337",
   "appId": "1:712659313071:web:93421a6b271635cdbe0f5a",
-  "storageBucket": "",
+  "storageBucket": "studio-9555481115-a8337.appspot.com",
   "apiKey": "AIzaSyBabyQ-kCBUyi3BtRfTN-Dfu574Uf5oqpc",
   "authDomain": "studio-9555481115-a8337.firebaseapp.com",
   "measurementId": "",
