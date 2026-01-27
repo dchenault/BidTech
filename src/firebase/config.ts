7bf664b64a1d73b0be1436af31d2ea78d7e4efe0
Dave Chenault <webschemes.net@gmail.com>
2026-01-27 01:53:05 +0000

    config.ts updates to storage URL

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9555481115-a8337",
   "appId": "1:712659313071:web:93421a6b271635cdbe0f5a",
-  "storageBucket": "studio-9555481115-a8337.appspot.com",
+  "storageBucket: "studio-9555481115-a8337.firebasestorage.app",
   "apiKey": "AIzaSyBabyQ-kCBUyi3BtRfTN-Dfu574Uf5oqpc",
   "authDomain": "studio-9555481115-a8337.firebaseapp.com",
   "measurementId": "",
