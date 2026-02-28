26ee337af09c7105e4d5d54aabd2da2a3cde1365
Dave Chenault <webschemes.net@gmail.com>
2026-02-28 21:32:46 +0000

    Removed the invited folder

--- src/app/invite/[inviteId]/page.tsx
+++
@@ -1,8 +0,0 @@
-// This folder/file is deprecated and should be deleted to resolve the route slug conflict.
-// All logic has been consolidated into src/app/invite/[token]/page.tsx.
-
-import { redirect } from 'next/navigation';
-
-export default function DeprecatedInvitePage({ params }: { params: { inviteId: string } }) {
-  redirect(`/invite/${params.inviteId}`);
-}
